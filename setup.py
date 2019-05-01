f4424a317862b8ec96a06b0942f8db1d3f662a0c
xynx59 <30712943+xynx59@users.noreply.github.com>
2019-05-01 18:58:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyPLS",
-    version="0.0.1",
+    version="0.0.2",
     author="Renjian Pan and Xingyue Ding",
     author_email="xynx29@gmail.com",
-    description="A function for PLSR",
+    description="A function for PLSR. Just update the KernelPLS method",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/xynx59/PyPLS",
