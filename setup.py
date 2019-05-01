a89cffafc7d8a14a727c632eada864b430092857
xynx59 <30712943+xynx59@users.noreply.github.com>
2019-05-01 00:09:58 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyPLS",
+    version="0.0.1",
+    author="Renjian Pan and Xingyue Ding",
+    author_email="xynx29@gmail.com",
+    description="A function for PLSR",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xynx59/PyPLS",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+]
